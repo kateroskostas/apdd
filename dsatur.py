07baa9d5682263da7df92d76344fb2604d7e830a
ΚΩΝΣΤΑΝΤΙΝΟΣ ΚΑΤΕΡΟΣ <kateros_konstantinos@hotmail.com>
2021-09-19 15:40:18 +0300

    οκ

--- dsatur.py
+++ dsatur.py
@@ -1,8 +1,10 @@
 from networkx import Graph
 import networkx as nx
 
-
+# Δημιουργώ μια συνάρτηση για να κάνω το χρωματισμό γράφου
 def color_dsatur(graph: Graph):
+    # δημιουργώ ενα κενό λεξικό για να καταχωρίσω μετα την λύση μου
     solution = dict()
+    # Κάνω τον χρωματισμό του γράφου μου επιλέγοντας την τεχνική DSATUR
     solution = nx.coloring.greedy_color(graph, strategy="DSATUR")
     return solution
--- save_my_solutions.py
+++ save_my_solutions.py
@@ -1,6 +1,13 @@
+# Δημιουργώ μια συνάρτηση για να αποθηκεύσω την λύση μου
 def save_solution(problem_name, solution, periods):
+    # Αντικαθηστώ το όνομα του αρχείου μου .stu αφαιρώντας την κατάληξη
     name_without_suffix = problem_name.replace(".stu", "")
+    # Αποθηκεύω με f string ονομα αρχείου_αριθμό απο περιόδους που μας έσωσε η λύση μας .sol
     save_name = f"{name_without_suffix}_{periods}.sol"
+
+    # Με ανοιχτό το αρχείο μας και με δικαιώματα εγγραφής αυτή την φορά
     with open(save_name, "w") as file:
+        # Για κάθε κλειδί που βλέπει στο dictionary solution
         for key in solution:
+            # Γράφει μια γραμμή key δηλαδή exam Tab solution key δηλαδή την περίοδο και μετά αλλάζει γραμμή
             file.write(f"{key}\t{solution[key]}\n")
